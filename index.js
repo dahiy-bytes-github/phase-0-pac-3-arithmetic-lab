a64537c0884595b3d365be344b075b5fe46e5efd
Abdullahi <aadahiye@gmail.com>
2024-09-08 10:45:45 +0300

    completes JavaScript Calculator Challenge Lab

---
+++ index.js
@@ -0,0 +1,36 @@
+function add(a,b){
+    const sum = a+b;
+    return sum;
+
+}
+function subtract(a,b){
+    const difference = a-b;
+    return difference;
+}
+function multiply(a,b){
+    const product = a*b;
+    return product;
+
+}
+function divide(a,b){
+    const quotient = a/b;
+    return quotient
+
+}
+function increment(n){
+   return ++n;
+
+}
+function  decrement(n){
+    return --n
+}
+function makeInt(n){
+    return parseInt(n)
+}
+function makeInt(n){
+    return parseInt(n,10)
+}
+function preserveDecimal(n) {
+    return parseFloat(n);
+  }
+  
